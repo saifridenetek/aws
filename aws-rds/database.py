cc96346d06aab82f73eb1289b21b300fc4e9df26
saifridenetek <saifridenetek@gmail.com>
2025-11-20 10:42:37 +0100

    configure db

--- aws-rds/database.py
+++ aws-rds/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user_name:password@endpoint/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://saifridenetek:SEPTSEGh12!@database-1.czk6k8cycan4.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
